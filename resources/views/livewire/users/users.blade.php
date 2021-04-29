5b4e4f5cf48022600898500507f3f094b47c6eaa
Francisco <navasfran98@gmail.com>
2021-04-29 01:04:32 -0600

    componente user

--- resources/views/livewire/users/users.blade.php
+++ resources/views/livewire/users/users.blade.php
@@ -1,5 +1,22 @@
 <div>
-  @php
-    print_r($users);
-  @endphp
+  <div class="col-md-12">
+    <table class="table">
+      <thead class="thead-dark">
+        <tr>
+          <th scope="col">#</th>
+          <th scope="col">First</th>
+          <th scope="col">Last</th>
+          <th scope="col">Handle</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <th scope="row">1</th>
+          <td>Mark</td>
+          <td>Otto</td>
+          <td>@mdo</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
 </div>
--- resources/views/user/users.blade.php
+++ resources/views/user/users.blade.php
@@ -1,5 +1,9 @@
 <x-app-layout>
-
+<div class="container">
+  <div class="row">
+    @livewire('Users.Users')
+  </div>
+</div>
 
 
 </x-app-layout>
